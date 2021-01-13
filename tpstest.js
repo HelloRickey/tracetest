1fbbe92e9acd08d82df03881b26b86e59a61d0ab
rickey <383523743@qq.com>
2021-01-13 13:12:12 +0800

    Update tpstest.js

--- tpstest.js
+++ tpstest.js
@@ -13,22 +13,21 @@ const network = new Network.Network(
   LEDGER_PORT
 );
 
-async function createAsset(){
-    const aliceKeyPairStr =
-      "eb5399f890043df675daeb1567f9c5f2acc6193d07da87251ab3bce8980fd4e38f2d4cc590f6d036dd83eea2eeb0b84127e896071805c3bc40fe162b447c18de";
-    const aliceKeyPair = FindoraWasm.keypair_from_str(aliceKeyPairStr);
-    const tokenCode = FindoraWasm.random_asset_type();
-    const memo = "this is a test asset";
-    const assetRules = FindoraWasm.AssetRules.new();
-    let blockCount = BigInt((await network.getStateCommitment())[1]);
-    console.log(blockCount);
-    const definitionTransaction = FindoraWasm.TransactionBuilder.new(blockCount)
-      .add_operation_create_asset(aliceKeyPair, memo, tokenCode, assetRules)
-      .transaction();
-    const result=await network.submitTransaction(definitionTransaction);
-    console.log(result);
+async function createAsset() {
+  const aliceKeyPairStr =
+    "eb5399f890043df675daeb1567f9c5f2acc6193d07da87251ab3bce8980fd4e38f2d4cc590f6d036dd83eea2eeb0b84127e896071805c3bc40fe162b447c18de";
+  const aliceKeyPair = FindoraWasm.keypair_from_str(aliceKeyPairStr);
+ const tokenCode = FindoraWasm.random_asset_type();
+  const memo = "this is a test asset";
+  const assetRules = FindoraWasm.AssetRules.new();
+  let blockCount = BigInt((await network.getStateCommitment())[1]);
+  const definitionTransaction = FindoraWasm.TransactionBuilder.new(blockCount)
+    .add_operation_create_asset(aliceKeyPair, memo, tokenCode, assetRules)
+    .transaction();
+  const result = await network.submitTransaction(definitionTransaction);
+  console.log("Block Height:" + blockCount);
+  console.log("Create Asset Result：" + result);
 }
-
 setInterval(function () {
   createAsset();
 }, 1000);
